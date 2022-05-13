72703f12eca2afe07f18c73c9c52352e076161b6
Megashark404 <83326299+Megashark404@users.noreply.github.com>
2022-05-13 22:26:09 +0900

    Update params.php

--- common/config/params.php
+++ common/config/params.php
@@ -6,6 +6,7 @@ return [
     'senderName' => 'Example.com mailer',
     'feedbackReceiverEmail' => 'admin@positron.test.task',
     'bookParseUrl' => 'https://gitlab.com/prog-positron/test-app-vacancy/-/raw/master/books.json',
+    'booksPerPage' => 20,
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
 ];
