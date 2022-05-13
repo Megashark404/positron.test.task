7b166c014b1554a5f639a3afaa778db0f9a97025
Megashark404 <83326299+Megashark404@users.noreply.github.com>
2022-05-13 22:20:06 +0900

    Update params.php

--- common/config/params.php
+++ common/config/params.php
@@ -5,6 +5,7 @@ return [
     'senderEmail' => 'noreply@positron.test.task',
     'senderName' => 'Example.com mailer',
     'feedbackReceiverEmail' => 'admin@positron.test.task',
+    'bookParseUrl' => 'https://gitlab.com/prog-positron/test-app-vacancy/-/raw/master/books.json',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
 ];
